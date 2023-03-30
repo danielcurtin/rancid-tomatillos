e1db8828fad186757aa1ac9863f870ae89e3463b
Daniel Curtin <dcurtin.work@gmail.com>
2023-03-30 16:15:54 -0600

    Refactor single movie styling and small css changes otherwise

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -38,17 +38,19 @@ class SingleMovie extends Component {
             return
         }
         return (
-            <section className="singleView">             
-                <div className="singlePoster">
-                    <img src={this.state.movie.poster_path} />
-                    <h2>{this.state.movie.average_rating} / 10</h2>
-                </div>
-                <div className="singleDetails">
-                    <img src={this.state.movie.backdrop_path} />
-                    <MovieDesc title={this.state.movie.title} tagline={this.state.movie.tagline} description={this.state.movie.overview} />
-                    <MovieTags runtime={this.state.movie.runtime} genres={this.state.movie.genres} budget={this.state.movie.budget} revenue={this.state.movie.revenue} id={this.state.movie.id} />
-                </div>
-            </section>
+            <>
+                <img src={this.state.movie.backdrop_path} className="backdropImg"/>
+                <section className="singleView">             
+                    <div className="singlePoster">
+                        <img src={this.state.movie.poster_path} />
+                        <h2>{this.state.movie.average_rating} / 10</h2>
+                    </div>
+                    <div className="singleDetails">
+                        <MovieDesc title={this.state.movie.title} tagline={this.state.movie.tagline} description={this.state.movie.overview} />
+                        <MovieTags runtime={this.state.movie.runtime} genres={this.state.movie.genres} budget={this.state.movie.budget} revenue={this.state.movie.revenue} id={this.state.movie.id} />
+                    </div>
+                </section>
+            </>
         );
     }
 };
